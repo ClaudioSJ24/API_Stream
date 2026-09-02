cc58f0ceba03c31fb4c6e1520975ac44a3929028
Claudio Sanchez Juarez <sanjuacla@gmail.com>
2026-09-02 11:35:50 -0600

    Initil commit

---
+++ src/test/java/com/juarez/stream_API/StreamApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.juarez.stream_API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StreamApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
