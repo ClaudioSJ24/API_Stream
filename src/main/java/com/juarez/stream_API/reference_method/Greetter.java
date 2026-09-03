05508879af4f3114e266a421e5c0b69d8a16455c
Claudio Sanchez Juarez <sanjuacla@gmail.com>
2026-09-03 10:34:47 -0600

    Reference method exercise

---
+++ src/main/java/com/juarez/stream_API/reference_method/Greetter.java
@@ -0,0 +1,11 @@
+package com.juarez.stream_API.reference_method;
+
+public class Greetter {
+
+    public Greetter() {}
+
+    public void sayHello(String who) {
+        IO.println("Hello " + who + "!");
+    }
+
+}
